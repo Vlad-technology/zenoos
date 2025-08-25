b07085d46d8e747d670ecec5b914b917c67142d1
Vlad-technology <vpk060312@gmail.com>
2025-08-25 20:02:15 +0300

    first normal commit

---
+++ fully edition/DelArch/obj.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	///"bufio"
+	"fmt"
+	////"os"
+	////"io"
+)
+
+func main() {
+	fmt.Println("                                   ZENOOS FULLY EDITION")
+	/////var reader = bufio.NewReader(os.Stdin)
+	////var read
+while:
+	fmt.Print("Zenoos>")
+	///read = reader.ReadString('\n')
+	var read1 string
+	fmt.Scan(&read1)
+	fmt.Println(read1)
+	goto while
+}
