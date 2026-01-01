5c0cf1ab73e3c06e78d7f4cc958b6614cae81396
Vlad-technology <vpk060312@gmail.com>
2026-01-01 04:06:28 +0300

    NEW ARDUINO EDITION

---
+++ arduino edition/srcandino/zeno.ino
@@ -0,0 +1,11 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+void setup() {
+  lcd.begin(16, 2);
+  lcd.print("Zenoos");
+  lcd.setCursor(0, 1);
+  lcd.print("ARDUINO EDITION");
+}
+void loop() {
+
+}
